f9df442a464f37877e3d9d70721ba3bba561c65b
sebastiands1234 <sebastiands1234@icloud.com>
2020-12-21 22:28:45 +0000

    Finished shapes and colors and tashe me. Working on logical turtle

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,29 +5,40 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # This code makes a new Turtle. Pick a new name for the turtle
-    myTurtle = turtle.Turtle()
+    Sebastian = turtle.Turtle()
     
     # Make your turtle's shape 'turtle', .shape('turtle')
-    
+    Sebastian.shape('turtle')
     # Set your turtle's speed using .speed(2)
-    
+    Sebastian.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    Sebastian.color('blue')
     # Move your turtle forward using .forward(100)
+    Sebastian.forward(100)
     # TEST    Did your turtle move forward?
     
     # Move your turtle left or right using .left(90) or .right(90)
-    
+
     # Now put the forward and left/right code into a for loop to repeat 4 times.
+    for i in range(4):
+        Sebastian.left(90)
+        Sebastian.forward(100)
+
     # TEST    Did your turtle draw a square?
-        
+    Sebastian.penup()
     # Move your turtle to a new place on the screen using .goto(x, y)
+    Sebastian.goto(90,180)
     # x=0 and y=0 is the center of the screen
     
     # Have your turtle draw a circle using .circle(radius, steps=30)
+    Sebastian.pendown()
+    Sebastian.begin_fill()
+    Sebastian.circle(100,360,100)
+    Sebastian.end_fill()
     # TEST    Did your turtle draw a circle?
     
     # Add color to your shape by adding .begin_fill() before drawing the circle
+
     # and .end_fill() below
 
     # Draw 3 more shapes with different fill colors!
--- _01_drawing/_b_tashe_me.py
+++ _01_drawing/_b_tashe_me.py
@@ -14,7 +14,7 @@ def setBackground(filename):
 
 def addMoustache(filename):
     m = turtle.Turtle()
-    
+
     m.hideturtle()
     window.addshape(filename)
     m.shape(filename)
@@ -30,21 +30,21 @@ def screenClicked(x, y):
     
     # 4. Show your moustache by calling the .showturtle() function
     # moustache.showturtle()
-    
+    moustache.showturtle()
     # 5. Move your moustache to a new location using .goto(x, y)
-
+    moustache.goto(x,y)
 if __name__ == '__main__':
     window = turtle.Screen()
     
     # 1. Find an image of a face online that you want to put a moustache on and
     #    add the file to the folder with your code in _01_drawing_ 
-    
+
     # 2. Call the setBackground() function with the image filename inside of the parenthesis
     # setBackground('emoji.png')
-    
+    setBackground('emoji2.gif')
     # 3. Create a variable called moustache and set it equal to addMoustache('moustache1.gif')
     # moustache = addMoustache('moustache1.gif')
-
+    moustache = addMoustache('moustache1.gif')
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screenClicked)
     turtle.done()
